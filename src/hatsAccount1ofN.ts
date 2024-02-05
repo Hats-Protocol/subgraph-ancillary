d0c1346bfbdcfb666993b09d0e222c96fe1d7690
Ido <gershido@gmail.com>
2024-02-05 15:27:24 +0200

    clean

--- src/hatsAccount1ofN.ts
+++ src/hatsAccount1ofN.ts
@@ -4,7 +4,6 @@ import {
   HatsAccount1ofN,
   HatsAccount1ofNTransaction,
 } from "../generated/schema";
-import { HATS } from "./constants";
 
 export function handleTxExecuted(event: TxExecuted): void {
   const hatsAccount1ofN = HatsAccount1ofN.load(
@@ -43,32 +42,6 @@ export function handleTxExecuted(event: TxExecuted): void {
 
   // batch execution
   if (event.transaction.input.toHexString().slice(0, 10) == "0x43629a73") {
-    //const toParam = ethereum.Value.fromAddress(
-    //  Address.fromString("0x3bc1A0Ad72417f2d411118085256fC53CBdDd137")
-    //);
-    //const valueParam = ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(5));
-    //const dataParam = ethereum.Value.fromBytes(Bytes.fromHexString("0x1234"));
-    //const operationParam = ethereum.Value.fromI32(1);
-    //const arr: Array<ethereum.Value> = [
-    //  toParam,
-    //  valueParam,
-    //  dataParam,
-    //  operationParam,
-    //];
-    //const tuple = changetype<ethereum.Tuple>(arr);
-    //const tupleArray = [tuple];
-    //const tupleArrayValue = ethereum.Value.fromTupleArray(tupleArray);
-    //const encoded = ethereum.encode(tupleArrayValue) as Bytes;
-    //log.info("encoded : {}", [encoded.toHexString()]);
-
-    //const decoded = ethereum.decode(
-    //  "(address,uint256,bytes,uint8)[]",
-    //  encoded
-    //) as ethereum.Value;
-    //const decodedTupleArray = decoded.toTupleArray<ethereum.Tuple>();
-    //const decodedTuple = decodedTupleArray[0];
-    //log.info("val 1: {}", [decodedTuple[0].toAddress().toHexString()]);
-
     const preparedCallData = Bytes.fromHexString(
       event.transaction.input.toHexString().slice(10)
     );
