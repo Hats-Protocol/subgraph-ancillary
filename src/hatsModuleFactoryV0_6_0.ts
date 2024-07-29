785d3fef3f235590500a8901ffaf3ec20d064295
Ido <gershido@gmail.com>
2024-07-29 15:11:31 +0300

    adiitional modules

--- src/hatsModuleFactoryV0_6_0.ts
+++ src/hatsModuleFactoryV0_6_0.ts
@@ -14,6 +14,12 @@ import {
   HatsStakingShaman as HatsStakingShamanObject,
   ShamanStake,
   HatsFarcasterDelegator as HatsFarcasterDelegatorObject,
+  Erc20Eligibility as Erc20EligibilityObject,
+  Erc721Eligibility as Erc721EligibilityObject,
+  Erc1155Eligibility as Erc1155EligibilityObject,
+  HatWearingEligibility as HatWearingEligibilityObject,
+  GitcoinPassportEligibility as GitcoinPassportEligibilityObject,
+  CoLinksEligibility as CoLinksEligibilityObject,
 } from "../generated/schema";
 import {
   JokeRaceEligibility as JokeRaceEligibilityTemplate,
@@ -27,6 +33,12 @@ import {
   AgreementEligibility as AgreementEligibilityTemplate,
   HatsStakingShaman as HatsStakingShamanTemplate,
   HatsFarcasterDelegator as HatsFarcasterDelegatorTemplate,
+  Erc20Eligibility as Erc20EligibilityTemplate,
+  Erc721Eligibility as Erc721EligibilityTemplate,
+  Erc1155Eligibility as Erc1155EligibilityTemplate,
+  HatWearingEligibility as HatWearingEligibilityTemplate,
+  GitcoinPassportEligibility as GitcoinPassportEligibilityTemplate,
+  CoLinksEligibility as CoLinksEligibilityTemplate,
 } from "../generated/templates";
 import {
   JOKERACE_ELIGIBILITY_IMPLEMENTATION,
@@ -41,10 +53,22 @@ import {
   AGREEMENT_ELIGIBILITY_IMPLEMENTATION,
   HATS_STAKING_SHAMAN_IMPLEMENTATION,
   HATS_FARCASTER_DELEGATOR_IMPLEMENTATION,
+  ERC20_ELIGIBILITY_IMPLEMENTATION,
+  ERC721_ELIGIBILITY_IMPLEMENTATION,
+  ERC1155_ELIGIBILITY_IMPLEMENTATION,
+  HAT_WEARING_ELIGIBILITY_IMPLEMENTATION,
+  GITCOIN_PASSPORT_ELIGIBILITY_IMPLEMENTATION,
+  COLINKS_ELIGIBILITY_IMPLEMENTATION,
 } from "./constants";
 import { HatsStakingShaman as HatsStakingShamanContract } from "../generated/templates/HatsStakingShaman/HatsStakingShaman";
 import { HatsFarcasterDelegator as HatsFarcasterDelegatorContract } from "../generated/templates/HatsFarcasterDelegator/HatsFarcasterDelegator";
 import { StakingEligibility as StakingEligibilityContract } from "../generated/templates/StakingEligibility/StakingEligibility";
+import { Erc20Eligibility as Erc20EligibilityContract } from "../generated/templates/Erc20Eligibility/Erc20Eligibility";
+import { Erc721Eligibility as Erc721EligibilityContract } from "../generated/templates/Erc721Eligibility/Erc721Eligibility";
+import { Erc1155Eligibility as Erc1155EligibilityContract } from "../generated/templates/Erc1155Eligibility/Erc1155Eligibility";
+import { HatWearingEligibility as HatWearingEligibilityContract } from "../generated/templates/HatWearingEligibility/HatWearingEligibility";
+import { GitcoinPassportEligibility as GitcoinPassportEligibilityContract } from "../generated/templates/GitcoinPassportEligibility/GitcoinPassportEligibility";
+import { CoLinksEligibility as CoLinksEligibilityContract } from "../generated/templates/CoLinksEligibility/CoLinksEligibility";
 import { hatIdToHex, getLinkedTreeAdmin } from "./utils";
 
 export function handleModuleDeployed(
@@ -485,6 +509,104 @@ export function handleModuleDeployed(
     hatsFarcasterDelegator.caster = hatIdToHex(casterHat);
     hatsFarcasterDelegator.owner = hatIdToHex(ownerHat);
     hatsFarcasterDelegator.save();
+  } else if (implemenatationAddress == ERC20_ELIGIBILITY_IMPLEMENTATION) {
+    Erc20EligibilityTemplate.create(event.params.instance);
+    const erc20Eligibility = new Erc20EligibilityObject(
+      event.params.instance.toHexString()
+    );
+    const erc20EligibilityContract = Erc20EligibilityContract.bind(
+      event.params.instance
+    );
+
+    const token = erc20EligibilityContract.ERC20_TOKEN_ADDRESS();
+    const hatId = erc20EligibilityContract.hatId();
+
+    erc20Eligibility.token = token.toHexString();
+    erc20Eligibility.hatId = hatIdToHex(hatId);
+    erc20Eligibility.save();
+  } else if (implemenatationAddress == ERC721_ELIGIBILITY_IMPLEMENTATION) {
+    Erc721EligibilityTemplate.create(event.params.instance);
+    const erc721Eligibility = new Erc721EligibilityObject(
+      event.params.instance.toHexString()
+    );
+    const erc721EligibilityContract = Erc721EligibilityContract.bind(
+      event.params.instance
+    );
+
+    const token = erc721EligibilityContract.ERC721_TOKEN_ADDRESS();
+    const hatId = erc721EligibilityContract.hatId();
+
+    erc721Eligibility.token = token.toHexString();
+    erc721Eligibility.hatId = hatIdToHex(hatId);
+    erc721Eligibility.save();
+  } else if (implemenatationAddress == ERC1155_ELIGIBILITY_IMPLEMENTATION) {
+    Erc1155EligibilityTemplate.create(event.params.instance);
+    const erc1155Eligibility = new Erc1155EligibilityObject(
+      event.params.instance.toHexString()
+    );
+    const erc1155EligibilityContract = Erc1155EligibilityContract.bind(
+      event.params.instance
+    );
+
+    const token = erc1155EligibilityContract.TOKEN_ADDRESS();
+    const hatId = erc1155EligibilityContract.hatId();
+    const numTokens = erc1155EligibilityContract.ARRAY_LENGTH();
+    const tokenIds = erc1155EligibilityContract.TOKEN_IDS();
+    const minBalances = erc1155EligibilityContract.MIN_BALANCES();
+
+    erc1155Eligibility.contractAddress = token.toHexString();
+    erc1155Eligibility.hatId = hatIdToHex(hatId);
+    erc1155Eligibility.tokens = tokenIds.slice(0, numTokens.toI32());
+    erc1155Eligibility.minBalances = minBalances.slice(0, numTokens.toI32());
+    erc1155Eligibility.save();
+  } else if (implemenatationAddress == HAT_WEARING_ELIGIBILITY_IMPLEMENTATION) {
+    HatWearingEligibilityTemplate.create(event.params.instance);
+    const hatWearingEligibility = new HatWearingEligibilityObject(
+      event.params.instance.toHexString()
+    );
+    const hatWearingEligibilityContract = HatWearingEligibilityContract.bind(
+      event.params.instance
+    );
+
+    const hatId = hatWearingEligibilityContract.hatId();
+    const criterionHat = hatWearingEligibilityContract.CRITERION_HAT();
+
+    hatWearingEligibility.hatId = hatIdToHex(hatId);
+    hatWearingEligibility.criterionHat = hatIdToHex(criterionHat);
+    hatWearingEligibility.save();
+  } else if (
+    implemenatationAddress == GITCOIN_PASSPORT_ELIGIBILITY_IMPLEMENTATION
+  ) {
+    GitcoinPassportEligibilityTemplate.create(event.params.instance);
+    const gitcoinPassportEligibility = new GitcoinPassportEligibilityObject(
+      event.params.instance.toHexString()
+    );
+    const gitcoinPassportEligibilityContract =
+      GitcoinPassportEligibilityContract.bind(event.params.instance);
+
+    const hatId = gitcoinPassportEligibilityContract.hatId();
+    const decoder = gitcoinPassportEligibilityContract.gitcoinPassportDecoder();
+    const scoreCriterion = gitcoinPassportEligibilityContract.scoreCriterion();
+
+    gitcoinPassportEligibility.hatId = hatIdToHex(hatId);
+    gitcoinPassportEligibility.decoder = decoder.toHexString();
+    gitcoinPassportEligibility.scoreCriterion = scoreCriterion;
+    gitcoinPassportEligibility.save();
+  } else if (implemenatationAddress == COLINKS_ELIGIBILITY_IMPLEMENTATION) {
+    CoLinksEligibilityTemplate.create(event.params.instance);
+    const coLinksEligibility = new CoLinksEligibilityObject(
+      event.params.instance.toHexString()
+    );
+    const coLinksEligibilityContract = CoLinksEligibilityContract.bind(
+      event.params.instance
+    );
+
+    const hatId = coLinksEligibilityContract.hatId();
+    const threshold = coLinksEligibilityContract.THRESHOLD();
+
+    coLinksEligibility.hatId = hatIdToHex(hatId);
+    coLinksEligibility.threshold = threshold;
+    coLinksEligibility.save();
   }
 }
 
