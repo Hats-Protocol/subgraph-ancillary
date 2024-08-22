cae0cd077e8b1fe768b3e6c5fc6f4d7a95e1a89b
Ido <gershido@gmail.com>
2024-08-22 11:26:20 +0200

    staking eligibility events

--- src/modules/stakingEligibility.ts
+++ src/modules/stakingEligibility.ts
@@ -12,9 +12,18 @@ import {
   StakingEligibility,
   HatAuthority,
   Stake,
+  Staking_ForgivenEvent,
+  Staking_CooldownPeriodChangedEvent,
+  Staking_JudgeHatChangedEvent,
+  Staking_MinStakeChangedEvent,
+  Staking_RecipientHatChangedEvent,
+  Staking_SlashedEvent,
+  Staking_StakedEvent,
+  Staking_UnstakeBegunEvent,
 } from "../../generated/schema";
 import { hatIdToHex } from "../utils";
 import { BigInt } from "@graphprotocol/graph-ts";
+import { createEventID } from "./utils";
 
 export function handleJudgeHatChanged(
   event: StakingEligibility_JudgeHatChanged
@@ -32,6 +41,18 @@ export function handleJudgeHatChanged(
   }
 
   stakingEligibility.judgeHat = hatIdToHex(event.params.newJudgeHat);
+
+  const judgeHatChangedEvent = new Staking_JudgeHatChangedEvent(
+    createEventID(event, "JudgeHatChangedEvent")
+  );
+  judgeHatChangedEvent.module = stakingEligibility.id;
+  judgeHatChangedEvent.stakingEligibilityInstance = stakingEligibility.id;
+  judgeHatChangedEvent.blockNumber = event.block.number.toI32();
+  judgeHatChangedEvent.timestamp = event.block.timestamp;
+  judgeHatChangedEvent.transactionID = event.transaction.hash;
+  judgeHatChangedEvent.newJudgeHat = hatIdToHex(event.params.newJudgeHat);
+
+  judgeHatChangedEvent.save();
   stakingEligibility.save();
   judgeHatAuthority.save();
 }
@@ -54,6 +75,20 @@ export function handleRecipientHatChanged(
   }
 
   stakingEligibility.recipientHat = hatIdToHex(event.params.newRecipientHat);
+
+  const recipientHatChangedEvent = new Staking_RecipientHatChangedEvent(
+    createEventID(event, "RecipientHatChanged")
+  );
+  recipientHatChangedEvent.module = stakingEligibility.id;
+  recipientHatChangedEvent.stakingEligibilityInstance = stakingEligibility.id;
+  recipientHatChangedEvent.blockNumber = event.block.number.toI32();
+  recipientHatChangedEvent.timestamp = event.block.timestamp;
+  recipientHatChangedEvent.transactionID = event.transaction.hash;
+  recipientHatChangedEvent.newRecipientHat = hatIdToHex(
+    event.params.newRecipientHat
+  );
+
+  recipientHatChangedEvent.save();
   stakingEligibility.save();
   recipientHatAuthority.save();
 }
@@ -81,6 +116,18 @@ export function handleStaked(event: StakingEligibility_Staked): void {
     stake.amount = currentStake.plus(event.params.amount);
   }
 
+  const stakedEvent = new Staking_StakedEvent(
+    createEventID(event, "StakedEvent")
+  );
+  stakedEvent.module = stakingEligibility.id;
+  stakedEvent.stakingEligibilityInstance = stakingEligibility.id;
+  stakedEvent.blockNumber = event.block.number.toI32();
+  stakedEvent.timestamp = event.block.timestamp;
+  stakedEvent.transactionID = event.transaction.hash;
+  stakedEvent.staker = event.params.staker.toHexString();
+  stakedEvent.amount = event.params.amount;
+
+  stakedEvent.save();
   stake.save();
   stakingEligibility.save();
 }
@@ -99,6 +146,19 @@ export function handleUnstakeBegun(
   stake.cooldownEndsAt = event.params.cooldownEnd;
   stake.amount.minus(event.params.amount);
 
+  const unstakeBegunEvent = new Staking_UnstakeBegunEvent(
+    createEventID(event, "UnstakeBegun")
+  );
+  unstakeBegunEvent.module = stakingEligibility.id;
+  unstakeBegunEvent.stakingEligibilityInstance = stakingEligibility.id;
+  unstakeBegunEvent.blockNumber = event.block.number.toI32();
+  unstakeBegunEvent.timestamp = event.block.timestamp;
+  unstakeBegunEvent.transactionID = event.transaction.hash;
+  unstakeBegunEvent.staker = event.params.staker.toHexString();
+  unstakeBegunEvent.amount = event.params.amount;
+  unstakeBegunEvent.cooldownEnd = event.params.cooldownEnd;
+
+  unstakeBegunEvent.save();
   stake.save();
   stakingEligibility.save();
 }
@@ -118,6 +178,18 @@ export function handleSlashed(event: StakingEligibility_Slashed): void {
   stakingEligibility.totalSlashedStakes =
     stakingEligibility.totalSlashedStakes.plus(event.params.amount);
 
+  const slashedEvent = new Staking_SlashedEvent(
+    createEventID(event, "Slashed")
+  );
+  slashedEvent.module = stakingEligibility.id;
+  slashedEvent.stakingEligibilityInstance = stakingEligibility.id;
+  slashedEvent.blockNumber = event.block.number.toI32();
+  slashedEvent.timestamp = event.block.timestamp;
+  slashedEvent.transactionID = event.transaction.hash;
+  slashedEvent.wearer = event.params.wearer.toHexString();
+  slashedEvent.amount = event.params.amount;
+
+  slashedEvent.save();
   stake.save();
   stakingEligibility.save();
 }
@@ -130,6 +202,18 @@ export function handleMinStakeChanged(
   ) as StakingEligibility;
 
   stakingEligibility.minStake = event.params.newMinStake;
+
+  const minStakeChangedEvent = new Staking_MinStakeChangedEvent(
+    createEventID(event, "MinStakeChanged")
+  );
+  minStakeChangedEvent.module = stakingEligibility.id;
+  minStakeChangedEvent.stakingEligibilityInstance = stakingEligibility.id;
+  minStakeChangedEvent.blockNumber = event.block.number.toI32();
+  minStakeChangedEvent.timestamp = event.block.timestamp;
+  minStakeChangedEvent.transactionID = event.transaction.hash;
+  minStakeChangedEvent.newMinStake = event.params.newMinStake;
+
+  minStakeChangedEvent.save();
   stakingEligibility.save();
 }
 
@@ -141,6 +225,18 @@ export function handleCooldownPeriodChanged(
   ) as StakingEligibility;
 
   stakingEligibility.cooldownPeriod = event.params.newDelay;
+
+  const cooldownPeriodChangedEvent = new Staking_CooldownPeriodChangedEvent(
+    createEventID(event, "CooldownPeriodChanged")
+  );
+  cooldownPeriodChangedEvent.module = stakingEligibility.id;
+  cooldownPeriodChangedEvent.stakingEligibilityInstance = stakingEligibility.id;
+  cooldownPeriodChangedEvent.blockNumber = event.block.number.toI32();
+  cooldownPeriodChangedEvent.timestamp = event.block.timestamp;
+  cooldownPeriodChangedEvent.transactionID = event.transaction.hash;
+  cooldownPeriodChangedEvent.newDelay = event.params.newDelay;
+
+  cooldownPeriodChangedEvent.save();
   stakingEligibility.save();
 }
 
@@ -155,6 +251,18 @@ export function handleForgiven(event: StakingEligibility_Forgiven): void {
   stake.slashed = false;
 
   stake.save();
+
+  const forgivenEvent = new Staking_ForgivenEvent(
+    createEventID(event, "Forgiven")
+  );
+  forgivenEvent.module = stakingEligibility.id;
+  forgivenEvent.stakingEligibilityInstance = stakingEligibility.id;
+  forgivenEvent.blockNumber = event.block.number.toI32();
+  forgivenEvent.timestamp = event.block.timestamp;
+  forgivenEvent.transactionID = event.transaction.hash;
+  forgivenEvent.staker = event.params.staker.toHexString();
+
+  forgivenEvent.save();
   stakingEligibility.save();
 }
 
