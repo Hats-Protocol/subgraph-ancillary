75ccd3d45e7d54df2b36f52e6f9d36f2a0c9ce43
Ido <gershido@gmail.com>
2024-12-04 11:50:26 +0200

    fix handleModuleProxyCreation

--- src/moduleProxyFactory.ts
+++ src/moduleProxyFactory.ts
@@ -6,13 +6,28 @@ import { HatsSignerGateV2 as HatsSignerGateV2Contract } from "../generated/templ
 import { hatIdToHex } from "./utils";
 
 export function handleModuleProxyCreation(event: ModuleProxyCreation): void {
-  HatsSignerGateV2Template.create(event.params.proxy);
-  let hsg = new HatsSignerGateV2(event.params.proxy.toHexString());
-  const hsgContract = HatsSignerGateV2Contract.bind(event.params.proxy);
+  if (
+    event.params.masterCopy.toHexString() ==
+    "0x61A5182A0958961E5be8Dc4174c9bE9C5EB6f384".toLowerCase()
+  ) {
+    HatsSignerGateV2Template.create(event.params.proxy);
+    let hsg = new HatsSignerGateV2(event.params.proxy.toHexString());
+    const hsgContract = HatsSignerGateV2Contract.bind(event.params.proxy);
 
-  const safe = hsgContract.safe();
+    const safe = hsgContract.safe();
 
-  hsg.safe = safe.toHexString();
-  hsg.implementation = event.params.masterCopy.toHexString();
-  hsg.save();
+    hsg.safe = safe.toHexString();
+    hsg.implementation = event.params.masterCopy.toHexString();
+    hsg.ownerHat = hatIdToHex(BigInt.fromI32(0));
+    hsg.signerHats = [];
+    hsg.locked = false;
+    hsg.claimableFor = false;
+    hsg.enabledDelegatecallTargets = [];
+    hsg.thresholdType = "ABSOLUTE";
+    hsg.minThreshold = BigInt.fromI32(1);
+    hsg.targetThreshold = BigInt.fromI32(1);
+    hsg.guard = "0x0000000000000000000000000000000000000000";
+    hsg.modules = [];
+    hsg.save();
+  }
 }
